035d65a3b9170b9658c7f6cc2290bdb62c1fdd17
unknown <asiamcarter@gmail.com>
2019-04-30 10:05:07 -0500

    setting up identity

---
+++ WasteNotBE/WasteNotBE/Models/ApplicationUser.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WasteNotBE.Models
+{
+    public class ApplicationUser: IdentityUser
+    {  
+        public int Id { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Story")]
+        public string Story { get; set; }
+
+        [Display(Name = "Photo")]
+        public string PhotoURL { get; set; }
+
+        public string isAdmin { get; set; }
+    }
+}
---
+++ WasteNotBE/WasteNotBE/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WasteNotBE.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
