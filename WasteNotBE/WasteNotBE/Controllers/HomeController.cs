0cb8be9466c88a107cbe2866b006de46db8f5347
unknown <asiamcarter@gmail.com>
2019-04-30 21:00:01 -0500

    homepage now displays last 12 items added to the site and is lightly styled

--- WasteNotBE/WasteNotBE/Controllers/HomeController.cs
+++ WasteNotBE/WasteNotBE/Controllers/HomeController.cs
@@ -1,19 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WasteNotBE.Data;
 using WasteNotBE.Models;
+using WasteNotBE.Models.HomeViewModels;
 
 namespace WasteNotBE.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //Retrieve first 12 Items
         public IActionResult Index()
         {
-            return View();
+            List<Item> Items = _context.Items
+                .OrderByDescending(i => i.DateCreated) 
+                .Take(12) 
+                .ToList(); 
+
+            HomeIndexViewModel ViewModel = new HomeIndexViewModel(); 
+            ViewModel.ItemList = Items; 
+
+            return View(ViewModel); 
         }
+
 
         public IActionResult Privacy()
         {
