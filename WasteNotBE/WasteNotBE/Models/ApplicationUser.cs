3898cf70e49393b57a164766a683a9411932ce24
unknown <asiamcarter@gmail.com>
2019-05-07 14:37:14 -0500

    routing established

--- WasteNotBE/WasteNotBE/Models/ApplicationUser.cs
+++ WasteNotBE/WasteNotBE/Models/ApplicationUser.cs
@@ -10,8 +10,6 @@ namespace WasteNotBE.Models
     public class ApplicationUser: IdentityUser
     {  
 
-        
-
         [Required]
         [Display(Name = "First Name")]
         public string FirstName { get; set; }
